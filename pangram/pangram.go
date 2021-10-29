f29a3d45a6391a0ca5ed7039d46810b50dd209c0
Camilo Romero <camiloaromero23@gmail.com>
2021-10-29 11:10:56 -0500

    Include pangram exercise

---
+++ pangram/pangram.go
@@ -0,0 +1,13 @@
+package pangram
+
+import "strings"
+
+func IsPangram(s string) bool {
+	s = strings.ToLower(s)
+	for i := 'a'; i < 'z'; i++ {
+		if !strings.ContainsRune(s, i) {
+			return false
+		}
+	}
+	return true
+}
